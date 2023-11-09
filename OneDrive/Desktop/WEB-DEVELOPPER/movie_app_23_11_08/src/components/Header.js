1443a4475d70b56f929c2baa92e323cfdf25ef91
cyrielle26 <cyrielleperminjat@gmail.com>
2023-11-09 16:59:05 +0900

    header ui end

--- OneDrive/Desktop/WEB-DEVELOPPER/movie_app_23_11_08/src/components/Header.js
+++ OneDrive/Desktop/WEB-DEVELOPPER/movie_app_23_11_08/src/components/Header.js
@@ -1,5 +1,51 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { routes } from "../routes";
+
+const SHeader = styled.header`
+padding: 20px 5%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+a{
+    color: white;
+}
+`;
+const Logo = styled.div`
+font-size: 24px;
+font-weight:700;
+`;
+
+const Nav = styled.ul`
+display: flex;
+justify-content: space-between;
+align-items: center;
+font-size: 18px;
+font-weight:600;
+li{
+    margin-left: 60px;
+}
+`;
+
+
+
+
 export const Header = () => {
-    return <div>
+    return <SHeader>
+        <Logo>
+            <Link to={routes.home}>PNFLIX</Link>
+        </Logo>
+        <Nav>
+            <li>
+            <Link to={routes.home}>Home</Link>
+            </li>
+            <li>
+            <Link to={routes.search}>Search</Link>
+            </li>
+            <li>
+            <Link to={routes.home}>Home</Link>
+            </li>
+        </Nav>
         
-    </div>
+    </SHeader>
 }
--- OneDrive/Desktop/WEB-DEVELOPPER/movie_app_23_11_08/src/index.js
+++ OneDrive/Desktop/WEB-DEVELOPPER/movie_app_23_11_08/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
+import { GlobalStyled} from "./GlobalStyled";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyled/>
     <Router />
   </React.StrictMode>
 );
 
 
-reportWebVitals();
+
